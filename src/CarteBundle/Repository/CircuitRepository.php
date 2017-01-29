de7825b047e4d19412203879b794322c2999eac1
Alexandre FRADET <alex.fradet.1@gmail.com>
2017-01-30 00:29:32 +0100

    Working on mediaquery for circuit search engine

--- src/CarteBundle/Repository/CircuitRepository.php
+++ src/CarteBundle/Repository/CircuitRepository.php
@@ -12,4 +12,42 @@ use Doctrine\ORM\EntityRepository;
  */
 class CircuitRepository extends EntityRepository
 {
+    public function circuitSearching($data)
+    {
+        $qb= $this->createQueryBuilder('c');
+        $qb->select('c');
+        $search = ['category'];
+        foreach ($search as $criteria)
+        {
+            if(isset($data[$criteria])){
+                $qb->andWhere('c.'.$criteria.' = :'.$criteria.'')->setParameter($criteria, $data[$criteria]);
+            }
+        }
+        $searchadress = 'postalcode';
+        if (isset($data[$searchadress]))
+        {
+   /*         $i=0;
+            $req = "";
+            foreach($data[$searchadress] as $postcode){
+                if ($i > 0){
+                    $req.= " OR ";
+                }
+                $req.='l'.$searchadress.'= :item'.$i.'';
+                $qb->setParameter('item'.$i, $postcode);
+                $i++;
+            }
+            $qb->andWhere($req);
+            $qb->innerJoin('c.locations', 'l');*/
+            $qb->andWhere('l.'.$searchadress.' IN :'.$data[$searchadress]);
+            $qb->innerJoin('c.locations', 'l');
+        }
+        $searchrating = "notation";
+        if (isset($data[$searchrating])){
+            $qb->addSelect('AVG(n.rating) as avr');
+            $qb->having('avr >= :notation');
+            $qb->setParameter('notation', $data[$searchrating]);
+            $qb->innerJoin('c.notations', 'c');
+        }
+        return $qb->getQuery()->getResult();
+    }
 }
