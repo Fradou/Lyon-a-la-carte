696ba36c9f333db647b5507e4462903c4db930ce
Alexandre FRADET <alex.fradet.1@gmail.com>
2017-02-20 21:34:40 +0100

    Update git ignore to keep directory structure but not the files generated by gulp

--- web/js/generator.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){function e(e){e.find("input:text, input:password, input:file, select, textarea").val(""),e.find("input:radio, input:checkbox").removeAttr("checked").removeAttr("selected"),console.log("reset done")}$("#parameterReset").click(function(){e($("form[name=generator]"))})});
