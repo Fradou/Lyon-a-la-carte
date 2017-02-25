37ffc2fd362cfa0d57e4c88a1c58709da04c136c
Alexandre FRADET <alex.fradet.1@gmail.com>
2017-02-25 10:13:04 +0100

    Connexion to api working and transfer to db ok, but i don't have the location in my sources. Need to find other source

--- src/MigrationBundle/Command/GetOpendataCommand.php
+++ src/MigrationBundle/Command/GetOpendataCommand.php
@@ -4,6 +4,7 @@
 namespace MigrationBundle\Command;
 
 
+use MigrationBundle\Entity\Place;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,7 +14,7 @@ class GetOpendataCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this
-            ->setName('carte-getlyondatas')
+            ->setName('opendatas:pull')
             ->setDescription('Get and update datas on locations')
             ->setHelp('Use curl to get datas on locations from LyonOpenData API. No parameter or else needed.')
         ;
@@ -22,8 +23,47 @@ class GetOpendataCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output){
 
         // Call service to get all datas from opendatalyon API
-        $this->getContainer()->get('app.OpenLyon_getter')->getPlaces();
+        $rdatas = $this->getContainer()->get('app.OpenLyon_getter')->getPlaces();
+        $output->writeln("j'ai mes raws");
+        $output->writeln($this->varDumpToString($rdatas));
 
+        $sites = $rdatas["features"];
+
+        $output->writeln("j'ai mes raws 2 ");
+        $em = $this->getContainer()->get("Doctrine")->getManager();
+        foreach ($sites as $site) {
+            $localisation = new Place();
+            $localisation->setType($site['properties']['type']);
+            $localisation->setTypeDetail($site['properties']['type_detail']);
+            $localisation->setName($site['properties']['nom']);
+            $localisation->setAddress($site['properties']['adresse']);
+            $localisation->setPostalcode($site['properties']['codepostal']);
+            $localisation->setTown($site['properties']['commune']);
+            $localisation->setPhone($site['properties']['telephone']);
+            $localisation->setMail($site['properties']['email']);
+            $localisation->setWebsite($site['properties']['siteweb']);
+            $localisation->setFacebook($site['properties']['facebook']);
+            $localisation->setRank($site['properties']['classement']);
+            $localisation->setOpenhour($site['properties']['ouverture']);
+            $localisation->setRateclear($site['properties']['tarifsenclair']);
+            $localisation->setMinrate($site['properties']['tarifsmin']);
+            $localisation->setMaxrate($site['properties']['tarifsmax']);
+            $localisation->setProducer($site['properties']['producteur']);
+            $localisation->setLatitude($site['geometry']['coordinates'][1]);
+            $localisation->setLongitude($site['geometry']['coordinates'][0]);
+
+            $em->persist($localisation);
+        }
+        $em->flush();
+
+    }
+
+
+    function varDumpToString($var) {
+        ob_start();
+        var_dump($var);
+        $result = ob_get_clean();
+        return $result;
     }
 
 }
--- src/MigrationBundle/Service/PlaceApiGetter.php
+++ src/MigrationBundle/Service/PlaceApiGetter.php
@@ -17,12 +17,12 @@ class PlaceApiGetter
 
         // fix to allow HTTPS connections with incorrect certificates.
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 
         // Connexion parameters.
-        curl_setopt($ch, CURLOPT_URL, "https://download.data.grandlyon.com/ws/rdata/sit_sitra.sittourisme/all.json?maxfeatures=6000");
+        curl_setopt($ch, CURLOPT_URL, "https://download.data.grandlyon.com/ws/rdata/sit_sitra.sittourisme/all.json?maxfeatures=5");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-        $output = json_decode(curl_exec($ch));
+        $output = json_decode(curl_exec($ch), true);
         curl_close($ch);
 
         return $output;
